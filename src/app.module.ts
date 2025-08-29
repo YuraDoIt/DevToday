8ab8b8dd70e43d6591452482694f89f9e4a2f31f
YuraTs <yura9989@gmail.com>
2025-08-29 19:32:09 +0300

    remove start files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CountryModule } from './country/country.module';
 import { ConfigModule } from './config/config.module';
 import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
   imports: [CountryModule, CalendarModule, ConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
